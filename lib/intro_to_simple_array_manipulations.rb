afb656bdfb55c50360e4177c397a742d4692243a
lior Shine <shinelior@gmail.com>
2019-12-12 23:18:58 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -89,7 +89,7 @@ def using_push( colors_in_the_rainbow,next_color)
         
         def using_delete_at(famous_robots,x)
           #famous_robots = ["Johnny 5", "R2D2", "Robocop"]
-         deleted_robot = famous_robots.delete_at(2) 
+         deleted_robot = famous_robots.delete_at(x) 
          return deleted_robot
        end
        
